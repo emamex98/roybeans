37d7c5f3dca6d9e6b2905f733036dc223d33a46b
Emanuel Estrada Larios <emamex98@gmail.com>
2019-03-13 11:09:25 -0600

    Universal footer, changes to homepage

--- js/menu.js
+++ js/menu.js
@@ -16,7 +16,7 @@ function addElementToMenu(title,linksTo) {
   var menuItems = [
     ["INICIO", "index.html"],
     ["NOSOTROS", "02_about_us.html"],
-    ["MENU", "03_menu.html"],
+    ["MENÚ", "03_menu.html"],
     ["UBICACIÓN", "05_contact.html"],
     ["PROMOCIONES", "04_BLOG.html"]
   ];
@@ -35,6 +35,14 @@ linkCTA.title = 'Ordenar';
 linkCTA.classList = 'plr-20 color-white btn-fill-primary';
 
 btCTA.appendChild(linkCTA);
+
+var d = new Date();
+var n = d.getFullYear();
+
+var footer = document.getElementById("footerContent");
+var footerHTML = '<div class="row"> <div class="col-md-4"> <div class="pt-30"> <p class="underline-secondary"><b>Horario:</b></p><p>Lunes a domingo:</p><p>1:00 - 11:00 pm.</p></div></div><div class="col-md-4"> <div class="pt-30"> <p class="underline-secondary"><b>Dirección:</b></p><p>Avenida Santa Margarita 3740,<br>Col. Poniente, Zapopan, Jalisco.<br>C.P. 45136 - Local 20</p></div></div><div class="col-md-4"> <div class="pt-30"> <p class="underline-secondary mb-10"><b>Contacto:</b></p><p>Teléfono: <a href="tel:+523324569009">(33) 2456 9009</a></p><p>Email: <a href="#">info@roybeans.com</a></p></div></div></div><div class="row"> <div class="col-lg-12"> <ul class="icon mt-30"> <li><a href="#"><i class="ion-social-facebook"></i></a></li><li><a href="#"><i class="ion-social-twitter"></i></a></li><li><a href="#"><i class="ion-social-instagram"></i></a></li></ul> </div></div><p class="color-light font-9 mt-50 mt-sm-30">Copyright &copy; ' + n + ' Roy Beans Pizza &amp; Salads | Desarrollado por <a href="http://ema.nuel.xyz">ema.nuel.xyz</a> | <a href="">Legal</a>'; 
+//This template is made with <i class="ion-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a></p>';
+footer.innerHTML = footerHTML;
 
 /*
 <h6><a class="plr-20 color-white btn-fill-primary" href="tel:+523324569009">ORDENA: (33) 2456 9009</a></h6>
